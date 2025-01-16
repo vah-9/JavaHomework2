8f07ec42549f3d4ba51f8e81cde37851061f4e67
Ivan Zhukhovitskiy <driveshuft@gmail.com>
2025-01-16 10:40:26 +0300

    Added some tests

--- src/main/java/org/example/com/Main.java
+++ src/main/java/org/example/com/Main.java
@@ -1,36 +1,49 @@
 package org.example.com;
 
-import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
+        Random random = new Random();
 
-        MyArrayList<Integer> a = new MyArrayList<Integer>();
-        MyArrayList<Integer> b = new MyArrayList<Integer>();
+        MyArrayList<Integer> a = new MyArrayList<>();
+        MyArrayList<String> b = new MyArrayList<>();
 
-        a.add(9); a.printAll();
-        a.add(2); a.printAll();
-        a.add(6); a.printAll();
-        b.add(2); b.printAll();
-        b.add(4); b.printAll();
-        b.add(1); b.printAll();
+        for (int i = 0; i < 1000; i++) {
+            a.add(random.nextInt(10000));
+        }
 
-        a.add(b); a.printAll();
+        for (int i = 0; i < 100; i++) {
+            a.remove(random.nextInt(100));
+        }
 
-        a.add(6,-12); a.printAll();
+        for (int i = 0; i < 50; i++) {
+            a.add(random.nextInt(100),random.nextInt(100));
+        }
 
-        a.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1-o2;
-            }
-        }); a.printAll();
-        a.remove(2); a.printAll();
-        a.clear(); a.printAll();
+        a.sort(Comparator.comparing(Integer::intValue));
+        a.printAll();
+        a.clear();
+        a.printAll();
+        for (int i = 0; i < 1000; i++) {
+
+            b.add( Integer.toString(random.nextInt(10000)));
+        }
+
+        for (int i = 0; i < 100; i++) {
+            b.remove(random.nextInt(100));
+        }
 
 
+        for (int i = 0; i < 50; i++) {
+            b.add(random.nextInt(100),Integer.toString(random.nextInt(100)));
+        }
+        b.sort(Comparator.comparing(String::toString));
+
+        b.printAll();
+        System.out.println(b.get(2));
 
     }
 }
